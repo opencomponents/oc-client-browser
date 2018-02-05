5721993c9cb6140e2ea3ee5e3cdba96f351e5b9e
Nick Balestra <nbalestra@opentable.com>
2018-02-05 12:09:00 +0000

    test logs

--- test/add-styles-to-head.js
+++ test/add-styles-to-head.js
@@ -5,7 +5,6 @@ describe('oc-client : addStylesToHead', function() {
     var styles = 'body: {background: red;}';
     it('should append a style tag with the correct content in the head', function() {
       oc.addStylesToHead(styles);
-      console.log(document.getElementsByTagName('style'));
       expect(document.getElementsByTagName('style')[0].textContent).toEqual(
         styles
       );
