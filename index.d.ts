8f42df6c4e7535e6bef7bd4d7c38b844cfbcff85
Ricardo Devis Agullo <mkredz@gmail.com>
2021-09-28 23:18:30 +0200

    change version field name

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@ type Callback<T> = (err: NodeJS.ErrnoException | null, data: T) => void;
 declare const ocClient: {
   getLib: (cb: Callback<string>) => void;
   getMap: (cb: Callback<string>) => void;
-  string: string;
+  version: string;
 };
 
 export = ocClient;
