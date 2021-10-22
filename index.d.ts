20be6c0666e90801d4059847d38ca0e86dd98604
Ricardo Devis Agullo <mkredz@gmail.com>
2021-10-22 13:23:34 +0200

    universalify exported methods

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,14 @@
 type Callback<T> = (err: NodeJS.ErrnoException | null, data: T) => void;
 
 declare const ocClient: {
-  getLib: (cb: Callback<string>) => void;
-  getMap: (cb: Callback<string>) => void;
+  getLib: {
+    (cb: Callback<string>): void;
+    (): Promise<string>;
+  };
+  getMap: {
+    (cb: Callback<string>): void;
+    (): Promise<string>;
+  };
   version: string;
 };
 
